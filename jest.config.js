6962f0788eaa3ead6b04635595cdd1402a6f329a
chromaticWaster <kabasi.freeman@gmail.com>
2022-08-10 13:15:21 +0200

    adding github actions reporter

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
     },
     transformIgnorePatterns: ['<rootDir>/node_modules/'],
     testPathIgnorePatterns: ['<rootDir>/dist/'],
-    reporters: ['default','jest-github-reporter']
+    reporters: ['default','github-actions']
   };
