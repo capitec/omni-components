499aa625542657c2fc3e99352a0b2b61d15c2304
Stefan Buys <10812446+stefan505@users.noreply.github.com>
2022-08-25 12:39:22 +0200

    Migrated to CSF 3

--- src/button/Button.stories.ts
+++ src/button/Button.stories.ts
@@ -1,30 +1,32 @@
-import { Meta, Story } from '@storybook/web-components';
+import { Meta, StoryContext } from '@storybook/web-components';
 import { userEvent, within } from '@storybook/testing-library';
 import { expect, jest } from '@storybook/jest';
 import { html } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { loadCssPropertiesRemote } from '../utils/StoryUtils.js';
-
 import { Button, ButtonType, buttonType, slotPositionType, SlotPositionType } from './Button.js';
 import './Button.js';
 import '../icon/Icon.js';
 
-// More on writing stories: https://storybook.js.org/docs/web-components/writing-stories/introduction
-
 export default {
     title: 'UI Components/Button',
     component: 'omni-button',
-    // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
     argTypes: {
-        type: { control: 'radio', options: buttonType },
-        slotPosition: { control: 'radio', options: slotPositionType }
+        type: {
+            control: 'radio',
+            options: buttonType,
+        },
+        slotPosition: {
+            control: 'radio',
+            options: slotPositionType,
+        },
     },
     parameters: {
         actions: {
-            handles: ['value-change']
+            handles: ['value-change'],
         },
-        cssprops: loadCssPropertiesRemote('omni-button')
-    }
+        cssprops: loadCssPropertiesRemote('omni-button'),
+    },
 } as Meta;
 
 interface Args {
@@ -38,10 +40,9 @@ interface Args {
 // INTERACTIVE
 // -----------
 
-// More templates and args: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
-
-const InteractiveTemplate: Story<Args> = (args: Args) => html`
-    <omni-button 
+export const Interactive = {
+    render: (args: Args) => html`
+    <omni-button
         data-testid="test-button"
         type="${args.type}"
         label="${ifDefined(args.label ? args.label : null)}"
@@ -49,112 +50,94 @@ const InteractiveTemplate: Story<Args> = (args: Args) => html`
         ?disabled=${args.disabled}>
         <omni-icon icon="@material/thumb_up"></omni-icon>
     </omni-button>
-`;
-
-export const Interactive = InteractiveTemplate.bind({});
-Interactive.storyName = 'Interactive';
-Interactive.parameters = {};
-Interactive.args = {
-    type: 'secondary',
-    label: 'Button',
-    slotPosition: 'top',
-    disabled: false
-};
-Interactive.play = async (context) => {
-    const button = within(context.canvasElement).getByTestId<Button>('test-button');
-    const click = jest.fn();
-    button.addEventListener('click', () => click());
-    await userEvent.click(button);
-    await userEvent.click(button);
-    await expect(click).toBeCalledTimes(2);
+  `,
+    name: 'Interactive',
+    args: {
+        type: 'secondary',
+        label: 'Button',
+        slotPosition: 'top',
+        disabled: false,
+    },
+    play: async (context: StoryContext) => {
+        const button = within(context.canvasElement).getByTestId<Button>('test-button');
+        const click = jest.fn();
+        button.addEventListener('click', () => click());
+        await userEvent.click(button);
+        await userEvent.click(button);
+        await expect(click).toBeCalledTimes(2);
+    },
 };
 
-// ----
-// TYPE
-// ----
-
-const TypeTemplate: Story<Args> = (args: Args) => html`
+export const Type = {
+    render: (args: Args) => html`
     <omni-button type="${args.type}" label="${args.label}" data-testid="test-button"></omni-button>
-`;
-
-export const Type = TypeTemplate.bind({});
-Type.storyName = 'Type';
-Type.args = {
-    type: 'primary',
-    label: 'Click'
+  `,
+    name: 'Type',
+    args: {
+        type: 'primary',
+        label: 'Click',
+    },
+    play: async (context: StoryContext) => {
+        const button = within(context.canvasElement).getByTestId<Button>('test-button');
+        const buttonElement = button.shadowRoot.getElementById('button');
+        const foundPrimaryClass = buttonElement.classList.contains('primary');
+        await expect(foundPrimaryClass).toBeTruthy();
+    },
 };
-Type.play = async (context) => {
-    const button = within(context.canvasElement).getByTestId<Button>('test-button');
-    const buttonElement = button.shadowRoot.getElementById('button');
-    const foundPrimaryClass = buttonElement.classList.contains('primary');
-    await expect(foundPrimaryClass).toBeTruthy();
-};
 
-// -----
-// LABEL
-// -----
-
-const LabelTemplate: Story<Args> = (args: Args) => html`
+export const Label = {
+    render: (args: Args) => html`
     <omni-button label="${args.label}" data-testid="test-button"></omni-button>
-`;
-
-export const Label = LabelTemplate.bind({});
-Label.storyName = 'Label';
-Label.args = {
-    label: 'Click'
+  `,
+    name: 'Label',
+    args: {
+        label: 'Click',
+    },
+    play: async (context: StoryContext) => {
+        const button = within(context.canvasElement).getByTestId<Button>('test-button');
+        const labelElement = button.shadowRoot.getElementById('label');
+        const labelMatches = labelElement.innerText === Label.args.label;
+        await expect(labelMatches).toBeTruthy();
+    },
 };
-Label.play = async (context) => {
-    const button = within(context.canvasElement).getByTestId<Button>('test-button');
-    const labelElement = button.shadowRoot.getElementById('label');
-    const labelMatches = labelElement.innerText === Label.args.label;
-    await expect(labelMatches).toBeTruthy();
-};
 
-// ----
-// SLOT
-// ----
-
-const SlotTemplate: Story<Args> = () => html`
+export const Slot = {
+    render: () => html`
     <omni-button data-testid="test-button">
-        <omni-icon size="default" icon="/assets/direction.svg"></omni-icon>
+      <omni-icon size="default" icon="/assets/direction.svg"></omni-icon>
     </omni-button>
-`;
-
-export const Slot = SlotTemplate.bind({});
-Slot.storyName = 'Slot';
-Slot.args = {};
-Slot.play = async (context) => {
-    const button = within(context.canvasElement).getByTestId<Button>('test-button');
-    const slotElement = button.shadowRoot.querySelector('slot');
-    const foundSlottedOmniIconElement = slotElement.assignedElements().find(e => e.tagName.toLowerCase() === 'omni-icon');
-    await expect(foundSlottedOmniIconElement).toBeTruthy();
+  `,
+    name: 'Slot',
+    args: {},
+    play: async (context: StoryContext) => {
+        const button = within(context.canvasElement).getByTestId<Button>('test-button');
+        const slotElement = button.shadowRoot.querySelector('slot');
+        const foundSlottedOmniIconElement = slotElement
+            .assignedElements()
+            .find((e) => e.tagName.toLowerCase() === 'omni-icon');
+        await expect(foundSlottedOmniIconElement).toBeTruthy();
+    },
 };
 
-// --------
-// DISABLED
-// --------
-
-const DisabledTemplate: Story<Args> = (args) => html`
+export const Disabled = {
+    render: (args: Args) => html`
     <omni-button disabled label="${args.label}" data-testid="test-button"></omni-button>
-`;
+  `,
+    name: 'Disabled',
+    args: {
+        label: 'Disabled',
+    },
+    play: async (context: StoryContext) => {
+        const button = within(context.canvasElement).getByTestId<Button>('test-button'); // Test for disabled CSS.
 
-export const Disabled = DisabledTemplate.bind({});
-Disabled.storyName = 'Disabled';
-Disabled.args = {
-    label: 'Disabled'
-};
-Disabled.play = async (context) => {
-    const button = within(context.canvasElement).getByTestId<Button>('test-button');
+        const buttonElement = button.shadowRoot.getElementById('button');
+        const foundDisabledClass = buttonElement.classList.contains('disabled');
+        await expect(foundDisabledClass).toBeTruthy(); // Test for not clickable.
 
-    // Test for disabled CSS.
-    const buttonElement = button.shadowRoot.getElementById('button');
-    const foundDisabledClass = buttonElement.classList.contains('disabled');
-    await expect(foundDisabledClass).toBeTruthy();
-
-    // Test for not clickable.
-    const click = jest.fn();
-    button.addEventListener('click', () => click());
-    await userEvent.click(button);
-    await userEvent.click(button);
-    await expect(click).toBeCalledTimes(0);
+        const click = jest.fn();
+        button.addEventListener('click', () => click());
+        await userEvent.click(button);
+        await userEvent.click(button);
+        await expect(click).toBeCalledTimes(0);
+    },
 };
