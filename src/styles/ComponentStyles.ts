ee62b8766e0d64c3778c083bb20d091eff8f3e6e
Stefan Buys <10812446+stefan505@users.noreply.github.com>
2022-09-01 12:20:43 +0200

    Added theme property descriptions

--- src/styles/ComponentStyles.ts
+++ src/styles/ComponentStyles.ts
@@ -1,36 +1,36 @@
 import { css } from 'lit';
 
 /** 
- * @cssprop --omni-theme-primary-color - The XXXX
- * @cssprop --omni-theme-primary-hover-color - The XXXX
- * @cssprop --omni-theme-primary-active-color - The XXXX
+ * @cssprop --omni-theme-primary-color - Theme primary color.
+ * @cssprop --omni-theme-primary-hover-color - Theme primary hover color.
+ * @cssprop --omni-theme-primary-active-color - Theme primary active color.
  * 
- * @cssprop --omni-theme-accent-color - The XXXX
- * @cssprop --omni-theme-accent-hover-color - The XXXX
- * @cssprop --omni-theme-accent-active-color - The XXXX
+ * @cssprop --omni-theme-accent-color - Theme accent color.
+ * @cssprop --omni-theme-accent-hover-color - Theme accent hover color.
+ * @cssprop --omni-theme-accent-active-color - Theme accent active color.
  * 
- * @cssprop --omni-theme-background-color - The XXXX
- * @cssprop --omni-theme-background-hover-color - The XXXX
- * @cssprop --omni-theme-background-active-color - The XXXX
+ * @cssprop --omni-theme-background-color - Theme background color.
+ * @cssprop --omni-theme-background-hover-color - Theme background hover color.
+ * @cssprop --omni-theme-background-active-color - Theme background active color.
  * 
- * @cssprop --omni-theme-font-color - The XXXX
+ * @cssprop --omni-theme-font-color - Theme font color.
  * 
- * @cssprop --omni-theme-disabled-border-color - The XXXX
- * @cssprop --omni-theme-disabled-background-color - The XXXX
- * @cssprop --omni-theme-error-font-color - The XXXX
- * @cssprop --omni-theme-error-border-color - The XXXX
- * @cssprop --omni-theme-hint-font-color - The XXXX
- * @cssprop --omni-theme-inactive-color - The XXXX
- * @cssprop --omni-theme-box-shadow-color - The XXXX
+ * @cssprop --omni-theme-disabled-border-color - Theme disabled border color.
+ * @cssprop --omni-theme-disabled-background-color - Theme disabled background color.
+ * @cssprop --omni-theme-error-font-color - Theme disabled background color.
+ * @cssprop --omni-theme-error-border-color - Theme error border color.
+ * @cssprop --omni-theme-hint-font-color - Theme hint font color.
+ * @cssprop --omni-theme-inactive-color - Theme inactive color.
+ * @cssprop --omni-theme-box-shadow-color - Theme inactive color.
  * 
- * @cssprop --omni-theme-font-family - The XXXX
- * @cssprop --omni-theme-font-size - The XXXX
- * @cssprop --omni-theme-font-weight - The XXXX
+ * @cssprop --omni-theme-font-family - Theme font family.
+ * @cssprop --omni-theme-font-size - Theme font size.
+ * @cssprop --omni-theme-font-weight - Theme font weight.
  * 
- * @cssprop --omni-theme-border-radius - The XXXX
- * @cssprop --omni-theme-border-width - The XXXX
+ * @cssprop --omni-theme-border-radius - Theme border radius.
+ * @cssprop --omni-theme-border-width - Theme border width.
  * 
- * @cssprop --omni-theme-box-shadow - The XXXX
+ * @cssprop --omni-theme-box-shadow - Theme box shadow.
  */
 export default css`
 
