5e5123306927a3b73802333c405df7e0ba11671e
BOTLANNER <16349308+BOTLANNER@users.noreply.github.com>
2022-09-15 15:21:17 +0200

    Updated build for esm

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,23 +1,23 @@
 import chalk from 'chalk';
 import { execSync } from 'child_process';
 import commandLineArgs from 'command-line-args';
-import { deleteSync } from 'del';
 import esbuild from 'esbuild';
-import fs from 'fs';
+import fs from 'fs-extra';
 import { globby } from 'globby';
-import copy from 'recursive-copy';
 
-const { bundle, dir, tsdir, verbose } = commandLineArgs([
+const { bundle, dir, tsdir, verbose, format, target } = commandLineArgs([
     { name: 'bundle', type: Boolean },
     { name: 'dir', type: String, defaultValue: 'dist' },
     { name: 'tsdir', type: String, defaultValue: 'dist' },
+    { name: 'format', type: String, defaultValue: 'esm' },
+    { name: 'target', type: String, defaultValue: 'es2017' },
     { name: 'verbose', type: Boolean }
 ]);
 
 const outdir = dir;
 
-deleteSync(outdir);
-deleteSync(tsdir);
+fs.rmSync(outdir, { recursive: true, force: true });
+fs.rmSync(tsdir, { recursive: true, force: true });
 fs.mkdirSync(tsdir, { recursive: true });
 
 (async () => {
@@ -35,14 +35,11 @@ fs.mkdirSync(tsdir, { recursive: true });
     // Copy the typescript output to an additional directory
     if (tsdir !== outdir) {
         if (verbose) console.log(chalk.bgYellow('Copying TypeScript output to build directory'));
-        await copy(tsdir, outdir);
+        await fs.copy(tsdir, outdir);
     }
 
-    const format = 'esm';
-    const target = 'es2017';
-
     console.log(`Building for ${format.toUpperCase()} ${target.toUpperCase()}...`);
-    const entryPoints = await globby('./src/**/!(*.(style|test|stories)).ts');
+    const entryPoints = await globby('./src/**/!(*.(style|test|stories)).(ts|js)');
     if (verbose) {
         console.log(chalk.bgYellow('Targeting the following entrypoints: \n'));
         entryPoints.forEach(e => console.log(chalk.bgYellow(`\t- ${e}`)));
@@ -53,7 +50,7 @@ fs.mkdirSync(tsdir, { recursive: true });
             target: target,
             entryPoints: entryPoints,
             outdir,
-            chunkNames: 'chunks/[ext]/[name].[hash]',
+            chunkNames: 'chunks-[ext]/[name].[hash]',
             incremental: false,
             define: {
                 // Floating UI requires this to be set
@@ -67,7 +64,12 @@ fs.mkdirSync(tsdir, { recursive: true });
             external: bundle ? [] : ['lit'],
             splitting: true,
             plugins: [],
-            logLevel: verbose ? 'debug' : 'info'
+            logLevel: verbose ? 'debug' : 'info',
+            minify: true,
+            sourcemap: 'linked',
+            sourcesContent: false
+            // May need to set this as the source maps are relative to dist by default
+            // sourceRoot: './'
         })
         .catch(err => {
             console.error(chalk.red(err));
