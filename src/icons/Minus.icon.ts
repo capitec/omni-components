42516860c15d38e2ab6faeec13e1187257dea2da
BOTLANNER <16349308+BOTLANNER@users.noreply.github.com>
2022-10-05 11:03:27 +0200

    - Updated plus and minus icons to use relevant base classes - Added plus and minus icons to Iconography docs

--- src/icons/Minus.icon.ts
+++ src/icons/Minus.icon.ts
@@ -1,95 +1,28 @@
-import { html, css, LitElement, TemplateResult } from 'lit';
+import { html, TemplateResult } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import ComponentStyles from '../styles/ComponentStyles';
+import DOMTreeElement from '../core/DOMTreeElement';
 
 /**
  * A minus icon component
  *
- * ```js 
- * import '@capitec/omni-components/icons/Minus.icon.js'; 
+ * ```js
+ * import '@capitec/omni-components/icons/Minus.icon.js';
  * ```
- * 
+ *
  * @example
- * 
+ *
  * ```html
  * <omni-minus-icon></omni-minus-icon>
  * ```
- * 
+ *
  * @element omni-minus-icon
- * 
+ *
  */
 @customElement('omni-minus-icon')
-export class MinusIcon extends LitElement {
-
-    // --------------
-    // INITIALISATION
-    // --------------
-
-    /**
-	 * @hideconstructor
-	 */
-    constructor() {
-
-        super();
-    }
-
-    // ------------------
-    // LIFECYCLE HANDLERS
-    // ------------------
-
-    // n/a
-
-    // ----------------
-    // PUBLIC FUNCTIONS
-    // ----------------
-
-    // n/a
-
-    // --------------
-    // EVENT HANDLERS
-    // --------------
-
-    // n/a
-
-    // ---------------
-    // PRIVATE METHODS
-    // ---------------
-
-    // n/a
-
-    // -------------------
-    // RENDERING TEMPLATES
-    // -------------------
-
-    /**
-	 * The element style template.
-	 * 
-	 */
-    static override get styles() {
-
-        return [
-            ComponentStyles,
-            css`
-				:host {
-					color: inherit;
-                    fill: inherit;
-                    background-color: inherit;
-                    background: inherit;
-
-                    width: var(--omni-icon-size, 16px);
-                    height: var(--omni-icon-size, 16px);
-				}
-			`
-        ];
-    }
-
-    /**
-	 * Apply changes to the element DOM when a property value changes.
-	 * 
-	 * @returns {TemplateResult} The updated DOM template.
-	 */
+export class MinusIcon extends DOMTreeElement {
     override render(): TemplateResult {
-        return html`  
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="100%" height="100%"><path d="M28.615 24c0 .35-.26.64-.598.686l-.094.006h-7.846a.692.692 0 0 1-.094-1.378l.094-.006h7.846c.382 0 .692.31.692.692Z"/></svg>`;
+        return html` <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="100%" height="100%">
+            <path d="M28.615 24c0 .35-.26.64-.598.686l-.094.006h-7.846a.692.692 0 0 1-.094-1.378l.094-.006h7.846c.382 0 .692.31.692.692Z" />
+        </svg>`;
     }
 }
