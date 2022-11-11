050fa2205f2807b6668fb9e8322583479deee75d
Stefan Buys <10812446+stefan505@users.noreply.github.com>
2022-11-11 09:48:28 +0200

    Filter fixes for attributes

--- scripts/eleventy/filters.js
+++ scripts/eleventy/filters.js
@@ -7,15 +7,24 @@ export function getTagName(value, componentName) {
 }
 
 export function getDescription(value, componentName) {
-    const component = loadCustomElementsModuleByFileFor(componentName, value);
-    const declaration = component.declarations.find(d => d.name === componentName);
+    const declaration = getComponentDeclaration(value, componentName);
     const description = declaration.description;
     return description;
 }
 
 export function getImport(value, componentName) {
-    const component = loadCustomElementsModuleByFileFor(componentName, value);
-    const declaration = component.declarations.find(d => d.name === componentName);
+    const declaration = getComponentDeclaration(value, componentName);
     const imp = declaration.import.replace(/```/gi, '').replace('js ', '').replace(/(\r\n|\n|\r)/gm, '');
     return imp;
+}
+
+export function getAttributes(value, componentName) {
+    const declaration = getComponentDeclaration(value, componentName);
+    return declaration.attributes;
+}
+
+export function getComponentDeclaration(value, componentName) {
+    const component = loadCustomElementsModuleByFileFor(componentName, value);
+    const declaration = component.declarations ? component.declarations.find(d => d.name === componentName) : null;
+    return declaration;
 }
