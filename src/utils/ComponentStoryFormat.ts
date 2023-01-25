ff19071e11bc2d757a4d451cae21256d211cf609
Stefan Buys <10812446+stefan505@users.noreply.github.com>
2023-01-25 12:10:14 +0200

    Added JSDoc header info for CSF type

--- src/utils/ComponentStoryFormat.ts
+++ src/utils/ComponentStoryFormat.ts
@@ -1,12 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PlayFunction } from './PlayFunction.js';
 
+/**
+ * This type intends to implement Component Story Format (CSF) v3.
+ * https://github.com/ComponentDriven/csf
+ */
 export type ComponentStoryFormat<T> = {
+
+    /** 
+     * Custom render function for the story. (standard) 
+     */
     render?: (args: T) => any;
+
+    /** 
+     * Suitable name for the story. (standard) 
+     */
     name?: string;
+
+    /** 
+     * Suitable description for the story. (non-standard) 
+     */
     description?: string | (() => any);
+
+    /**
+     * Dynamic set of arguments for the story to execute. (standard)
+     */
     args?: Partial<T>;
+
+    /**
+     * Test function for the story (standard)
+     */
     play?: PlayFunction<T>;
+
+    /**
+     * Obtain original DOM source (non-standard)
+     */
     source?: () => string;
 };
 
