2d48b4ebfb4c0161e8d210e7dece8693c39398f0
Stefan Buys <10812446+stefan505@users.noreply.github.com>
2023-04-25 14:45:06 +0200

    Fixed template code alignment

--- src/icon/Icon.stories.ts
+++ src/icon/Icon.stories.ts
@@ -72,7 +72,7 @@ export const SVG: ComponentStoryFormat<Args> = {
                   <path d="m8.229 14.062-3.521-3.541L5.75 9.479l2.479 2.459 6.021-6L15.292 7Z" />
                 </g>
               </svg>
-              `
+        `
     },
     play: async (context) => {
         const icon = within(context.canvasElement).getByTestId<Icon>('test-icon');
