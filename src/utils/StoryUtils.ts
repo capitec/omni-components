9fffa3da3c1bb9550538841724534ac49b1d579d
Stefan Buys <10812446+stefan505@users.noreply.github.com>
2023-06-02 15:54:45 +0200

    Style updates

--- src/utils/StoryUtils.ts
+++ src/utils/StoryUtils.ts
@@ -1366,7 +1366,6 @@ function setupGlobalSearch() {
 
                                     .search-item {
                                         display: flex;
-                                        color: var(--omni-theme-font-color);
                                         text-decoration: none;
                                         padding: 3px;
                                         cursor: pointer;
@@ -1451,8 +1450,8 @@ function setupGlobalSearch() {
             case 'story':
                 return svg`
                     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
-                        <path d="M0 0h24v24H0V0z" fill="none"/>
-                        <path d="M12 7.77L18.39 18H5.61L12 7.77M12 4L2 20h20L12 4z"/>
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M8 5v14l11-7z"/>
                     </svg>
                 `;
                 break;
