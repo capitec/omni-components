d64fdb7334a9078bf200a0065580c6da9bf17ce7
Stefan Buys <10812446+stefan505@users.noreply.github.com>
2023-06-06 19:42:42 +0200

    Resized codepen logo

--- src/utils/StoryRenderer.ts
+++ src/utils/StoryRenderer.ts
@@ -467,7 +467,7 @@ export class StoryRenderer extends LitElement {
                     @click="${() =>
                         this._generateCodePen((window.localStorage.getItem(frameworkStorageKey) ?? 'HTML') as FrameworkOption, frameworkSource)}">
                     <omni-icon size="default">
-                        <svg class="hidden-after-760" viewBox="0 0 138 26" fill="none" stroke="var(--omni-button-secondary-hover-border-color, currentColor)" stroke-width="2.3" stroke-linecap="round" stroke-linejoin="round" width="100%" height="100%" title="CodePen">
+                        <svg class="hidden-after-760" style="height: 22px;" viewBox="0 0 138 26" fill="none" stroke="var(--omni-button-secondary-hover-border-color, currentColor)" stroke-width="2.3" stroke-linecap="round" stroke-linejoin="round" width="100%" height="100%" title="CodePen">
                             <path d="M15 8a7 7 0 1 0 0 10m7-8.7L33 2l11 7.3v7.4L33 24l-11-7.3zm0 0 11 7.4 11-7.4m0 7.4L33 9.3l-11 7.4M33 2v7.3m0 7.4V24M52 6h5a7 7 0 0 1 0 14h-5zm28 0h-9v14h9m-9-7h6m11 1h6a4 4 0 0 0 0-8h-6v14m26-14h-9v14h9m-9-7h6m11 7V6l11 14V6"></path>
                         </svg>
                         <!--  -->
