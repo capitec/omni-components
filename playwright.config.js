e57c6add4aa2219c038e9c00e00e209744570ea6
BOTLANNER <16349308+BOTLANNER@users.noreply.github.com>
2023-06-26 08:08:10 +0200

    Enabled more browsers for testing

--- playwright.config.js
+++ playwright.config.js
@@ -64,46 +64,46 @@ const config = {
 
     /* Configure projects for major browsers */
     projects: [
-        // {
-        //     name: 'chromium',
-        //     use: {
-        //         ...devices['Desktop Chrome'],
-        //         userAgent: 'Test Runner'
-        //     },
-        // },
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome'],
+                userAgent: 'Test Runner'
+            },
+        },
 
-        // {
-        //     name: 'firefox',
-        //     use: {
-        //         ...devices['Desktop Firefox'],
-        //         userAgent: 'Test Runner'
-        //     },
-        // },
+        {
+            name: 'firefox',
+            use: {
+                ...devices['Desktop Firefox'],
+                userAgent: 'Test Runner'
+            },
+        },
 
-        // {
-        //     name: 'webkit',
-        //     use: {
-        //         ...devices['Desktop Safari'],
-        //         userAgent: 'Test Runner'
-        //     },
-        // },
+        {
+            name: 'webkit',
+            use: {
+                ...devices['Desktop Safari'],
+                userAgent: 'Test Runner'
+            },
+        },
 
-        // /* Test against mobile viewports. */
-        // {
-        //     name: 'Mobile Chrome',
-        //     use: {
-        //         ...devices['Pixel 5'],
-        //         userAgent: 'Test Runner'
-        //     },
-        // },
+        /* Test against mobile viewports. */
+        {
+            name: 'Mobile Chrome',
+            use: {
+                ...devices['Pixel 5'],
+                userAgent: 'Test Runner'
+            },
+        },
         
-        // {
-        //   name: 'Mobile Safari',
-        //   use: {
-        //     ...devices['iPhone 12'],
-        //         userAgent: 'Test Runner'
-        //   },
-        // },
+        {
+          name: 'Mobile Safari',
+          use: {
+            ...devices['iPhone 12'],
+                userAgent: 'Test Runner'
+          },
+        },
         
         {
           name: 'iPhone 13 Pro Max',
