efd8291147ac5ab78e5eb866a449c210b8428f0f
chromaticWaster <kabasi.freeman@gmail.com>
2023-08-15 16:07:52 +0200

    React story code formatting and Vue story updates

--- src/expander/Expander.stories.ts
+++ src/expander/Expander.stories.ts
@@ -69,16 +69,17 @@ export const Disabled: ComponentStoryFormat<Args> = {
     frameworkSources: [
         {
             framework: 'Vue',
-            load: (args) => getSourceFromLit(Interactive!.render!(args), undefined, (s) => s.replace(' disabled', ' :disabled="true"'))
+            load: (args) => getSourceFromLit(Disabled!.render!(args), undefined, (s) => s.replace(' disabled', ' :disabled="true"'))
         },
         {
             framework: 'React',
             load: (args) => `import { OmniExpander } from "@capitec/omni-components-react/expander";
-            import { OmniLabel } from "@capitec/omni-components-react/label";
-        
-            const App = () => <OmniExpander ${args.label ? ` label='${args.label}'` : ''} ${args.disabled ? ` disabled='${args.disabled}'` : ''}>
-             <OmniLabel label="I shall not be seen"></OmniLabel>
-            </OmniExpander>;`
+import { OmniLabel } from "@capitec/omni-components-react/label";
+
+const App = () => 
+<OmniExpander ${args.label ? ` label='${args.label}'` : ''} ${args.disabled ? ` disabled='${args.disabled}'` : ''}>
+    <OmniLabel label="I shall not be seen"></OmniLabel>
+</OmniExpander>;`
         }
     ],
     description: 'Prevent interaction (expanding/collapsing).',
@@ -102,13 +103,14 @@ export const Button_Alignment: ComponentStoryFormat<Args> = {
         {
             framework: 'React',
             load: (args) => `import { OmniExpander } from "@capitec/omni-components-react/expander";
-        import { OmniLabel } from "@capitec/omni-components-react/label";
-    
-        const App = () => <OmniExpander ${args.label ? ` label='${args.label}'` : ''} ${
-                args.buttonAlignment ? ` button-alignment='${args.buttonAlignment}'` : ''
-            }>
-         <OmniLabel label="The content of the expander"></OmniLabel>
-        </OmniExpander>;`
+import { OmniLabel } from "@capitec/omni-components-react/label";
+
+const App = () => 
+<OmniExpander ${args.label ? ` label='${args.label}'` : ''} ${
+        args.buttonAlignment ? ` button-alignment='${args.buttonAlignment}'` : ''
+    }>
+    <OmniLabel label="The content of the expander"></OmniLabel>
+</OmniExpander>;`
         }
     ],
     name: 'Button Alignment',
@@ -133,16 +135,17 @@ export const Expanded: ComponentStoryFormat<Args> = {
     frameworkSources: [
         {
             framework: 'Vue',
-            load: (args) => getSourceFromLit(Interactive!.render!(args), undefined, (s) => s.replace(' expanded', ' :expanded="true"'))
+            load: (args) => getSourceFromLit(Expanded!.render!(args), undefined, (s) => s.replace(' expanded', ' :expanded="true"'))
         },
         {
             framework: 'React',
             load: (args) => `import { OmniExpander } from "@capitec/omni-components-react/expander";
-            import { OmniLabel } from "@capitec/omni-components-react/label";
-        
-            const App = () => <OmniExpander ${args.label ? ` label='${args.label}'` : ''} ${args.expanded ? ` expanded='${args.expanded}'` : ''}>
-             <OmniLabel label="The content of the expander"></OmniLabel>
-            </OmniExpander>;`
+import { OmniLabel } from "@capitec/omni-components-react/label";
+
+const App = () => 
+<OmniExpander ${args.label ? ` label='${args.label}'` : ''} ${args.expanded ? ` expanded='${args.expanded}'` : ''}>
+    <OmniLabel label="The content of the expander"></OmniLabel>
+</OmniExpander>;`
         }
     ],
     description: 'Toggle expander expanded state',
@@ -163,12 +166,12 @@ export const Expand_Icon: ComponentStoryFormat<Args> = {
         {
             framework: 'React',
             load: (args) => `import { OmniExpander } from "@capitec/omni-components-react/expander";
-                import { OmniLabel } from "@capitec/omni-components-react/label";
-            
-                const App = () => <OmniExpander${args.label ? ` label='${args.label}'` : ''}>
-                 <svg slot="expand-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  width="24px" height="24px" style="fill: orange;"><path d="M12 2.25c5.385 0 9.75 4.365 9.75 9.75s-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12 6.615 2.25 12 2.25Zm0 1.5a8.25 8.25 0 1 0 0 16.5 8.25 8.25 0 0 0 0-16.5ZM8.47 8.47a.75.75 0 0 1 1.06 0L12 10.939l2.47-2.47a.75.75 0 0 1 .976-.072l.084.073a.75.75 0 0 1 0 1.06L13.061 12l2.47 2.47a.75.75 0 0 1 .072.976l-.073.084a.75.75 0 0 1-1.06 0L12 13.061l-2.47 2.47a.75.75 0 0 1-.976.072l-.084-.073a.75.75 0 0 1 0-1.06L10.939 12l-2.47-2.47a.75.75 0 0 1-.072-.976Z" /></svg>
-                 <OmniLabel label="The content of the expander"></OmniLabel>
-                </OmniExpander>;`
+import { OmniLabel } from "@capitec/omni-components-react/label";
+
+const App = () => <OmniExpander${args.label ? ` label='${args.label}'` : ''}>
+    <svg slot="expand-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  width="24px" height="24px" style="fill: orange;"><path d="M12 2.25c5.385 0 9.75 4.365 9.75 9.75s-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12 6.615 2.25 12 2.25Zm0 1.5a8.25 8.25 0 1 0 0 16.5 8.25 8.25 0 0 0 0-16.5ZM8.47 8.47a.75.75 0 0 1 1.06 0L12 10.939l2.47-2.47a.75.75 0 0 1 .976-.072l.084.073a.75.75 0 0 1 0 1.06L13.061 12l2.47 2.47a.75.75 0 0 1 .072.976l-.073.084a.75.75 0 0 1-1.06 0L12 13.061l-2.47 2.47a.75.75 0 0 1-.976.072l-.084-.073a.75.75 0 0 1 0-1.06L10.939 12l-2.47-2.47a.75.75 0 0 1-.072-.976Z" /></svg>
+    <OmniLabel label="The content of the expander"></OmniLabel>
+</OmniExpander>;`
         }
     ],
     name: 'Slotted Expand Icon',
@@ -189,12 +192,12 @@ export const Header_Icon: ComponentStoryFormat<Args> = {
         {
             framework: 'React',
             load: (args) => `import { OmniExpander } from "@capitec/omni-components-react/expander";
-            import { OmniLabel } from "@capitec/omni-components-react/label";
-        
-            const App = () => <OmniExpander${args.label ? ` label='${args.label}'` : ''}>
-             <svg slot="header-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  width="24px" height="24px" style="fill: orange;"><path d="M12 2.25c5.385 0 9.75 4.365 9.75 9.75s-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12 6.615 2.25 12 2.25Zm0 1.5a8.25 8.25 0 1 0 0 16.5 8.25 8.25 0 0 0 0-16.5ZM8.47 8.47a.75.75 0 0 1 1.06 0L12 10.939l2.47-2.47a.75.75 0 0 1 .976-.072l.084.073a.75.75 0 0 1 0 1.06L13.061 12l2.47 2.47a.75.75 0 0 1 .072.976l-.073.084a.75.75 0 0 1-1.06 0L12 13.061l-2.47 2.47a.75.75 0 0 1-.976.072l-.084-.073a.75.75 0 0 1 0-1.06L10.939 12l-2.47-2.47a.75.75 0 0 1-.072-.976Z" /></svg>
-             <OmniLabel label="The content of the expander"></OmniLabel>
-            </OmniExpander>;`
+import { OmniLabel } from "@capitec/omni-components-react/label";
+
+const App = () => <OmniExpander${args.label ? ` label='${args.label}'` : ''}>
+    <svg slot="header-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  width="24px" height="24px" style="fill: orange;"><path d="M12 2.25c5.385 0 9.75 4.365 9.75 9.75s-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12 6.615 2.25 12 2.25Zm0 1.5a8.25 8.25 0 1 0 0 16.5 8.25 8.25 0 0 0 0-16.5ZM8.47 8.47a.75.75 0 0 1 1.06 0L12 10.939l2.47-2.47a.75.75 0 0 1 .976-.072l.084.073a.75.75 0 0 1 0 1.06L13.061 12l2.47 2.47a.75.75 0 0 1 .072.976l-.073.084a.75.75 0 0 1-1.06 0L12 13.061l-2.47 2.47a.75.75 0 0 1-.976.072l-.084-.073a.75.75 0 0 1 0-1.06L10.939 12l-2.47-2.47a.75.75 0 0 1-.072-.976Z" /></svg>
+    <OmniLabel label="The content of the expander"></OmniLabel>
+</OmniExpander>;`
         }
     ],
     name: 'Slotted Header Icon',
